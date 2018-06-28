8d5df3afd5bc84ab1735744530accd3b7857120a
Ensar Kovankaya <ensar@kovankaya.com>
2018-06-28 18:07:11 +0300

    Add delete and getBy tests

--- app/src/services/user.service.spec.ts
+++ app/src/services/user.service.spec.ts
@@ -256,4 +256,139 @@ describe('UserService', () => {
         // Check is updated changed updatedAt
         expect(user.updatedAt.getTime()).to.not.eq(new Date("2018-06-26T19:22:06.755Z").getTime());
     });
+
+    it('should soft delete user', async () => {
+        const db = new FakeDatabase([
+            {
+                _id: "5b32925ea8b04a071c7f8bb0",
+                birthday: new Date("1999-01-26T00:00:00.000Z"),
+                gender: "MALE",
+                active: false,
+                deletedAt: null,
+                deleted: false,
+                lastLogin: null,
+                groups: [],
+                email: "nciccottio2r@pen.io",
+                firstName: "Nelson",
+                lastName: "Ciccottio",
+                role: "STUDENT",
+                password: "$2b$10$H210fn813wmANL5FSLz3re6og0xwJ0fKT4HqSY3hi.QgelGGQPM7.",
+                username: "nelsonciccottio",
+                createdAt: new Date("2018-06-26T19:22:06.755Z"),
+                updatedAt: new Date("2018-06-26T19:22:06.755Z"),
+                __v: 0
+            }
+        ]);
+        const now = new Date();
+        const service = new UserService(db);
+        await service.delete("5b32925ea8b04a071c7f8bb0");
+        const user = db.data[0];
+
+        expect(user).to.be.an('object');
+        expect(user.deleted).to.be.eq(true);
+        expect(user.deletedAt).to.be.a('date');
+        expect(user.deletedAt).to.be.gt(now);
+    });
+
+    it('should hard delete user', async () => {
+        const db = new FakeDatabase([
+            {
+                _id: "5b32925ea8b04a071c7f8bb0",
+                birthday: new Date("1999-01-26T00:00:00.000Z"),
+                gender: "MALE",
+                active: false,
+                deletedAt: null,
+                deleted: false,
+                lastLogin: null,
+                groups: [],
+                email: "nciccottio2r@pen.io",
+                firstName: "Nelson",
+                lastName: "Ciccottio",
+                role: "STUDENT",
+                password: "$2b$10$H210fn813wmANL5FSLz3re6og0xwJ0fKT4HqSY3hi.QgelGGQPM7.",
+                username: "nelsonciccottio",
+                createdAt: new Date("2018-06-26T19:22:06.755Z"),
+                updatedAt: new Date("2018-06-26T19:22:06.755Z"),
+                __v: 0
+            }
+        ]);
+        const service = new UserService(db);
+        await service.delete("5b32925ea8b04a071c7f8bb0", true);
+
+        expect(db.data.length).to.be.eq(0);
+    });
+
+    it('should get user by id, email and username', async () => {
+        const db = new FakeDatabase([
+            {
+                _id: "1",
+                birthday: new Date("1999-01-26T00:00:00.000Z"),
+                gender: "MALE",
+                active: false,
+                deletedAt: null,
+                deleted: false,
+                lastLogin: null,
+                groups: [],
+                email: "1@mail.com",
+                firstName: "Nelson",
+                lastName: "Ciccottio",
+                role: "STUDENT",
+                password: "$2b$10$H210fn813wmANL5FSLz3re6og0xwJ0fKT4HqSY3hi.QgelGGQPM7.",
+                username: "user1",
+                createdAt: new Date("2018-06-26T19:22:06.755Z"),
+                updatedAt: new Date("2018-06-26T19:22:06.755Z"),
+                __v: 0
+            },
+            {
+                _id: "2",
+                birthday: new Date("1999-01-26T00:00:00.000Z"),
+                gender: "MALE",
+                active: false,
+                deletedAt: null,
+                deleted: false,
+                lastLogin: null,
+                groups: [],
+                email: "2@mail.com",
+                firstName: "Nelson",
+                lastName: "Ciccottio",
+                role: "STUDENT",
+                password: "$2b$10$H210fn813wmANL5FSLz3re6og0xwJ0fKT4HqSY3hi.QgelGGQPM7.",
+                username: "user2",
+                createdAt: new Date("2018-06-26T19:22:06.755Z"),
+                updatedAt: new Date("2018-06-26T19:22:06.755Z"),
+                __v: 0
+            },
+            {
+                _id: "3",
+                birthday: new Date("1999-01-26T00:00:00.000Z"),
+                gender: "MALE",
+                active: false,
+                deletedAt: null,
+                deleted: false,
+                lastLogin: null,
+                groups: [],
+                email: "3@mail.com",
+                firstName: "Nelson",
+                lastName: "Ciccottio",
+                role: "STUDENT",
+                password: "$2b$10$H210fn813wmANL5FSLz3re6og0xwJ0fKT4HqSY3hi.QgelGGQPM7.",
+                username: "user3",
+                createdAt: new Date("2018-06-26T19:22:06.755Z"),
+                updatedAt: new Date("2018-06-26T19:22:06.755Z"),
+                __v: 0
+            }
+        ]);
+        const service = new UserService(db);
+        const byID = await service.getBy({id: "1"});
+        expect(byID._id).to.eq("1");
+
+        const byEmail = await service.getBy({email: "2@mail.com"});
+        expect(byEmail._id).to.eq("2");
+
+        const byUsername = await service.getBy({username: "user3"});
+        expect(byUsername._id).to.eq("3");
+
+        const notExists = await service.getBy({id: "5"});
+        expect(notExists).to.eq(null);
+    });
 });
