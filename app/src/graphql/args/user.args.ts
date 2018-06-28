90ff9521c457171b98284a49c3cb112466cb8e04
Ensar Kovankaya <ensar@kovankaya.com>
2018-06-29 00:44:49 +0300

    add description

--- app/src/graphql/args/user.args.ts
+++ app/src/graphql/args/user.args.ts
@@ -2,12 +2,12 @@ import { ArgsType, Field } from 'type-graphql';
 
 @ArgsType()
 export class UserArgs {
-    @Field({nullable: true})
+    @Field({nullable: true, description: 'Get user by id.'})
     public id?: string;
 
-    @Field({nullable: true})
+    @Field({nullable: true, description: 'Get user by username.'})
     public username?: string;
 
-    @Field({nullable: true})
+    @Field({nullable: true, description: 'Get user by email.'})
     public email?: string;
 }
