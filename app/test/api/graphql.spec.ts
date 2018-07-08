3fa8bba34f4335162b3accfb6945a37d079e41b5
Ensar Kovankaya <ensar@kovankaya.com>
2018-07-08 20:27:39 +0300

    Add create user with max data test

--- app/test/api/graphql.spec.ts
+++ app/test/api/graphql.spec.ts
@@ -1,9 +1,11 @@
 import { expect } from 'chai';
 import { GraphQLClient } from 'graphql-request';
 import * as http from 'http';
-import { after, before, describe, it } from 'mocha';
+import { after, before, beforeEach, describe, it } from 'mocha';
 import "reflect-metadata";
 import { Container } from 'typedi';
+import { Logger } from '../../src/logger';
+import { Gender, Role } from '../../src/models/user.model';
 import Server from '../../src/server';
 import { DatabaseService } from '../../src/services/database.service';
 import { MockDatabase } from '../unit/mock.database';
@@ -14,19 +16,41 @@ const headers = {
     'Accept': 'application/json'
 };
 
+const logger = new Logger('GraphQL Spec File');
+
 let server: http.Server;
 
 before('Start Server', async () => {
-    Container.set(DatabaseService, new MockDatabase());
     const PORT = parseInt(process.env.PORT || '3000', 10);
     const HOST = process.env.HOST || '0.0.0.0';
 
     server = http.createServer(Server);
-    return await server.listen(PORT, HOST, () => console.log(`Test Server start on host ${HOST} port ${PORT}.`));
+    return await server.listen(PORT, HOST, () => logger.info(`Test Server start on host ${HOST} port ${PORT}.`));
 });
 
 class ShouldNotSucceed extends Error {
     public name = 'ShouldNotSucceed';
+}
+
+beforeEach('Mock Database', () => Container.set(DatabaseService, new MockDatabase()));
+
+interface IUserModel {
+    _id?: string;
+    username?: string;
+    email?: string;
+    password?: string;
+    firstName?: string;
+    lastName?: string;
+    role?: Role;
+    gender?: Gender | null;
+    birthday?: string | null;
+    active?: boolean;
+    createdAt?: string;
+    updatedAt?: string;
+    deletedAt?: string | null;
+    deleted?: boolean;
+    lastLogin?: string | null;
+    groups?: string[];
 }
 
 describe('GraphQL', () => {
@@ -41,7 +65,7 @@ describe('GraphQL', () => {
         }
     });
 
-    describe('Endpoint -> create', () => {
+    describe('Create', () => {
         it("should throw error without variables", async () => {
             const client = new GraphQLClient(endpoint, {headers});
             const query = `mutation createUser($email: String!, $firstName: String!, $lastName: String!,
@@ -57,7 +81,7 @@ describe('GraphQL', () => {
                 await client.request(query);
                 throw new ShouldNotSucceed();  // Make sure raise error
             } catch (err) {
-                console.log('Error');
+                expect(err.name).to.not.eq('ShouldNotSucceed');
                 expect(err.response).to.be.an('object');
                 expect(err.response.status).to.eq(500);
                 expect(err.message)
@@ -73,46 +97,136 @@ describe('GraphQL', () => {
             }
         });
 
-        // it("should create user with min data", async () => {
-        //     const client = new GraphQLClient(endpoint, {headers});
-        //     const query = `mutation createUser($email: String!, $firstName: String!, $lastName: String!,
-        //                     $password: String!, $role: Role!, $username: String, $gender: Gender,
-        //                     $birthday: String, $groups: [String!]){
-        //             user: create(data: {email: $email, firstName: $firstName, lastName: $lastName, password: $password,
-        //                 role: $role, username: $username, gender: $gender, birthday: $birthday, groups: $groups}) {
-        //                     _id
-        //             }
-        //         }
-        //     `;
-        //     try {
-        //         await client.request(query, {
-        //             data: {
-        //                 email: 'email@example.com',
-        //                 firstName: 'FirstName',
-        //                 lastName: 'LastName',
-        //             }
-        //         });
-        //         throw new ShouldNotSucceed();
-        //     } catch (err) {
-        //         console.log('Error');
-        //         expect(err.response).to.be.an('object');
-        //         expect(err.response.status).to.eq(500);
-        //         expect(err.message)
-        //             .to.have.string('Variable \\"$email\\" of required type \\"String!\\" was not provided.');
-        //         expect(err.message)
-        //             .to.have.string('Variable \\"$firstName\\" of required type \\"String!\\" was not provided.');
-        //         expect(err.message)
-        //             .to.have.string('Variable \\"$lastName\\" of required type \\"String!\\" was not provided.');
-        //         expect(err.message)
-        //             .to.have.string('Variable \\"$password\\" of required type \\"String!\\" was not provided.');
-        //         expect(err.message)
-        //             .to.have.string('Variable \\"$role\\" of required type \\"Role!\\" was not provided.');
-        //     }
-        // });
+        it("should create user with min data", async () => {
+            const client = new GraphQLClient(endpoint, {headers});
+            const query = `mutation createUser($email: String!, $firstName: String!, $lastName: String!,
+                            $password: String!, $role: Role!, $username: String, $gender: Gender,
+                            $birthday: String, $active: Boolean, $groups: [String!]){
+                    user: create(data: {
+                        email: $email, firstName: $firstName, lastName: $lastName, password: $password, role: $role,
+                        username: $username, gender: $gender, birthday: $birthday, groups: $groups, active: $active
+                        }) {
+                            ...userFields
+                    }
+                }
+
+                fragment userFields on User {
+                    _id
+                    email
+                    firstName
+                    lastName
+                    role
+                    username
+                    gender
+                    birthday
+                    createdAt
+                    updatedAt
+                    deletedAt
+                    deleted
+                    groups
+                    active
+                    lastLogin
+                }
+            `;
+            const response = await client.request<{ user: IUserModel }>(query, {
+                email: 'email@example.com',
+                firstName: 'FirstName',
+                lastName: 'LastName',
+                password: '12345678',
+                role: 'ADMIN'
+            });
+            expect(response.user).to.be.an('object');
+            expect(response.user._id).to.be.a('string');
+            expect(response.user._id).to.have.lengthOf(24);
+            expect(response.user.email).to.be.eq('email@example.com');
+            expect(response.user.firstName).to.be.eq('FirstName');
+            expect(response.user.lastName).to.be.eq('LastName');
+            expect(response.user.password).to.be.eq(undefined);
+            expect(response.user.role).to.be.eq('ADMIN');
+            expect(response.user.username).to.be.a('string');
+            expect(response.user.gender).to.be.eq(null);
+            expect(response.user.birthday).to.be.eq(null);
+            expect(response.user.createdAt).to.be.a('string');
+            expect(new Date(response.user.createdAt).toString()).to.be.not.eq('Invalid Date');
+            expect(response.user.updatedAt).to.be.a('string');
+            expect(new Date(response.user.updatedAt).toString()).to.be.not.eq('Invalid Date');
+            expect(response.user.deletedAt).to.be.eq(null);
+            expect(response.user.deleted).to.be.eq(false);
+            expect(response.user.groups).to.be.an('array');
+            expect(response.user.groups).to.have.lengthOf(0);
+            expect(response.user.active).to.be.eq(true);
+            expect(response.user.lastLogin).to.be.eq(null);
+        });
+
+        it("should create user with max data", async () => {
+            const client = new GraphQLClient(endpoint, {headers});
+            const query = `mutation createUser($email: String!, $firstName: String!, $lastName: String!,
+                            $password: String!, $role: Role!, $username: String!, $gender: Gender!,
+                            $birthday: String!, $active: Boolean!, $groups: [String!]!){
+                    user: create(data: {
+                    email: $email, firstName: $firstName, lastName: $lastName, password: $password, role: $role,
+                    username: $username, gender: $gender, birthday: $birthday, active: $active, groups: $groups}) {
+                            ...userFields
+                    }
+                }
+
+                fragment userFields on User {
+                    _id
+                    email
+                    firstName
+                    lastName
+                    role
+                    username
+                    gender
+                    birthday
+                    createdAt
+                    updatedAt
+                    deletedAt
+                    deleted
+                    groups
+                    active
+                    lastLogin
+                }
+            `;
+            const response = await client.request<{ user: IUserModel }>(query, {
+                email: 'email2@example.com',
+                firstName: 'FirstName',
+                lastName: 'LastName',
+                password: '12345678',
+                role: 'ADMIN',
+                gender: 'MALE',
+                active: false,
+                groups: ['random-24-chars-group-id'],
+                birthday: new Date(1987, 3, 21),
+                username: 'customusername'
+            });
+            expect(response.user).to.be.an('object');
+            expect(response.user._id).to.be.a('string');
+            expect(response.user._id).to.have.lengthOf(24);
+            expect(response.user.email).to.be.eq('email2@example.com');
+            expect(response.user.firstName).to.be.eq('FirstName');
+            expect(response.user.lastName).to.be.eq('LastName');
+            expect(response.user.password).to.be.eq(undefined);
+            expect(response.user.role).to.be.eq('ADMIN');
+            expect(response.user.username).to.be.eq('customusername');
+            expect(response.user.gender).to.be.eq('MALE');
+            expect(response.user.birthday).to.be.a('string');
+            expect(new Date(response.user.birthday).toString()).to.be.not.eq('Invalid Date');
+            expect(response.user.createdAt).to.be.a('string');
+            expect(new Date(response.user.createdAt).toString()).to.be.not.eq('Invalid Date');
+            expect(response.user.updatedAt).to.be.a('string');
+            expect(new Date(response.user.updatedAt).toString()).to.be.not.eq('Invalid Date');
+            expect(response.user.deletedAt).to.be.eq(null);
+            expect(response.user.deleted).to.be.eq(false);
+            expect(response.user.groups).to.be.an('array');
+            expect(response.user.groups).to.have.lengthOf(1);
+            expect(response.user.active).to.be.eq(false);
+            expect(response.user.lastLogin).to.be.eq(null);
+        });
     });
 });
 
 after('Stop Server', () => server.close(() => {
-    console.log('Test Server closed.');
+    logger.info('Test Server closed.');
     process.exit();
 }));
