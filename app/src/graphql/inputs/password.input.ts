5a2c8e494a94c26115ddbea74d5e60f866ac0c05
Ensar Kovankaya <ensar@kovankaya.com>
2018-07-24 02:15:35 +0300

    add description

--- app/src/graphql/inputs/password.input.ts
+++ app/src/graphql/inputs/password.input.ts
@@ -1,16 +1,15 @@
-import { IsEmail, Length, Matches } from "class-validator";
+import { IsEmail, Length } from "class-validator";
 import { Field, InputType } from 'type-graphql';
 import { Validatable } from '../validatable';
 
-@InputType()
-export class PasswordArgs extends Validatable {
+@InputType({description: 'Check password input.'})
+export class PasswordInput extends Validatable {
     @Field({description: 'User email.'})
     @IsEmail({}, {message: 'InvalidEmail'})
     public email: string;
 
     @Field({description: 'User raw password.'})
     @Length(8, 32, {message: 'InvalidLength'})
-    @Matches(new RegExp('.*\\S.*', 'g'))
     public password: string;
 
     constructor(data = {}) {
