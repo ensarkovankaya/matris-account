fe97aad16a19676c1247fce4e16f2c6b2fe29253
Ensar Kovankaya <ensar@kovankaya.com>
2018-07-25 16:42:46 +0300

    add validate if optional fields

--- app/src/graphql/args/gender.query.ts
+++ app/src/graphql/args/gender.query.ts
@@ -1,4 +1,4 @@
-import { IsArray, IsIn } from "class-validator";
+import { IsArray, IsIn, ValidateIf } from "class-validator";
 import { Field, InputType } from 'type-graphql';
 import { IGenderQueryModel } from '../../models/gender.query.model';
 import { Gender } from '../../models/user.model';
@@ -7,10 +7,12 @@ import { Validatable } from '../validatable';
 @InputType({description: 'Gender Query'})
 export class GenderQuery extends Validatable implements IGenderQueryModel {
     @Field(type => Gender, {nullable: true, description: 'User gender equals to given value.'})
+    @ValidateIf((object, value) => value !== undefined)
     @IsIn([Gender.MALE, Gender.FEMALE, Gender.UNKNOWN], {message: 'Invalid'})
     public eq?: Gender;
 
     @Field(type => [Gender], {nullable: true, description: 'User gener is one of given values.'})
+    @ValidateIf((object, value) => value !== undefined)
     @IsArray()
     @IsIn([Gender.MALE, Gender.FEMALE, Gender.UNKNOWN], {message: 'Invalid', each: true})
     public in?: Gender[];
