16c62fdac2b08823ab126b150ca173e4b35a6d1a
Ensar Kovankaya <ensar@kovankaya.com>
2018-07-25 16:41:46 +0300

    add validate if for optional fields

--- app/src/graphql/args/user.args.ts
+++ app/src/graphql/args/user.args.ts
@@ -1,20 +1,23 @@
-import { IsAlphanumeric, IsEmail, IsLowercase, IsMongoId, Length } from "class-validator";
+import { IsAlphanumeric, IsEmail, IsLowercase, IsMongoId, Length, ValidateIf } from "class-validator";
 import { ArgsType, Field } from 'type-graphql';
 import { Validatable } from '../validatable';
 
 @ArgsType()
 export class UserArgs extends Validatable {
     @Field({nullable: true, description: 'Get user by id.'})
+    @ValidateIf((object, value) => value !== undefined)
     @IsMongoId()
     public id?: string;
 
     @Field({nullable: true, description: 'Get user by username.'})
+    @ValidateIf((object, value) => value !== undefined)
     @Length(4, 32, {message: 'InvalidLength'})
     @IsLowercase({message: 'NotLowercase'})
     @IsAlphanumeric({message: 'NotAlphanumeric'})
     public username?: string;
 
     @Field({nullable: true, description: 'Get user by email.'})
+    @ValidateIf((object, value) => value !== undefined)
     @IsEmail()
     public email?: string;
 
