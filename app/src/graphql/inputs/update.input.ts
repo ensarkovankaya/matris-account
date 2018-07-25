26d147e34148c5b995b2dea50ada3f1fa054754a
Ensar Kovankaya <ensar@kovankaya.com>
2018-07-25 16:47:29 +0300

    add validate if for optional fields

--- app/src/graphql/inputs/update.input.ts
+++ app/src/graphql/inputs/update.input.ts
@@ -6,7 +6,8 @@ import {
     IsIn,
     IsLowercase,
     Length,
-    Matches
+    Matches,
+    ValidateIf
 } from "class-validator";
 import { Field, InputType } from 'type-graphql';
 import { IsDateLike, IsInDateRange } from '../../decorators/date';
@@ -17,15 +18,18 @@ import { Validatable } from '../validatable';
 export class UpdateInput extends Validatable {
 
     @Field({nullable: true, description: 'User unique email address.'})
+    @ValidateIf((object, value) => value !== undefined)
     @IsEmail({}, {message: 'Invalid'})
     public email?: string;
 
     @Field({nullable: true, description: 'User first name. Must be between 2 and 32 characters.'})
+    @ValidateIf((object, value) => value !== undefined)
     @Matches(new RegExp('^[a-zA-Z ]+$', 'g'))
     @Length(2, 32, {message: 'InvalidLength'})
     public firstName?: string;
 
     @Field({nullable: true, description: 'User last name. Must be between 2 and 32 characters.'})
+    @ValidateIf((object, value) => value !== undefined)
     @Matches(new RegExp('^[a-zA-Z ]+$', 'g'))
     @Length(2, 32, {message: 'InvalidLength'})
     public lastName?: string;
@@ -34,6 +38,7 @@ export class UpdateInput extends Validatable {
         nullable: true,
         description: 'User unique username. If not provided will be generated. Must be between 4 and 32 characters.',
     })
+    @ValidateIf((object, value) => value !== undefined)
     @Length(4, 32, {message: 'InvalidLength'})
     @IsLowercase({message: 'NotLowercase'})
     @IsAlphanumeric({message: 'NotAlphanumeric'})
@@ -43,37 +48,40 @@ export class UpdateInput extends Validatable {
         nullable: true,
         description: 'User role. Can be one of SUPERADMIN, ADMIN, INSTRUCTOR, PARENT or STUDENT.'
     })
+    @ValidateIf((object, value) => value !== undefined)
     @IsIn([Role.ADMIN, Role.MANAGER, Role.INSTRUCTOR, Role.PARENT, Role.STUDENT], {message: 'Invalid'})
     public role?: Role;
 
     @Field({nullable: true, description: 'User password. Must be between 8 and 32 characters.'})
+    @ValidateIf((object, value) => value !== undefined)
     @Length(8, 32, {message: 'InvalidLength'})
     @Matches(new RegExp('.*\\S.*', 'g'))
     public password?: string;
 
     @Field({nullable: true, description: 'Is user active?'})
+    @ValidateIf((object, value) => value !== undefined)
     @IsBoolean()
     public active?: boolean;
 
     @Field(type => Gender, {nullable: true, description: 'User Gender. MALE or FEMALE'})
+    @ValidateIf((object, value) => value !== undefined)
     @IsIn([Gender.MALE, Gender.FEMALE, Gender.UNKNOWN])
     public gender?: Gender;
 
     @Field(type => String, {nullable: true, description: 'User birthday. Can be null if not defined.'})
+    @ValidateIf((object, value) => value !== undefined)
     @IsDateLike(true)
     @IsInDateRange(new Date(1950, 1, 1), new Date(2000, 12, 31))
     public birthday?: string | null;
 
     @Field(type => [String], {nullable: true, description: 'User associated group ids.'})
+    @ValidateIf((object, value) => value !== undefined)
     @IsArray()
     @Length(12, 24, {message: 'InvalidIDLength', each: true})
     public groups?: string[];
 
     @Field({nullable: true, description: 'If true updates user last login date to now.'})
+    @ValidateIf((object, value) => value !== undefined)
     @IsBoolean()
     public updateLastLogin?: boolean;
-
-    constructor(data: object = {}) {
-        super(data);
-    }
 }
