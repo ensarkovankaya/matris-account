5c603efb53633c5d6b236e964811eec43031adfd
Ensar Kovankaya <ensar@kovankaya.com>
2018-07-25 16:46:37 +0300

    add validate if for optional fields

--- app/src/graphql/inputs/pagination.input.ts
+++ app/src/graphql/inputs/pagination.input.ts
@@ -1,4 +1,4 @@
-import { IsIn, IsNumber, Max, Min } from "class-validator";
+import { IsIn, IsNumber, Max, Min, ValidateIf } from "class-validator";
 import { PaginateOptions } from 'mongoose';
 import { Field, InputType } from 'type-graphql';
 import { Validatable } from '../validatable';
@@ -7,19 +7,22 @@ import { Validatable } from '../validatable';
 export class PaginationInput extends Validatable implements PaginateOptions {
 
     @Field({nullable: true, description: 'Current page of query result.'})
+    @ValidateIf((object, value) => value !== undefined)
     @IsNumber()
     @Min(1)
     @Max(9999)
     public page?: number = 1;
 
     @Field({nullable: true, description: 'Current offset from data'})
+    @ValidateIf((object, value) => value !== undefined)
     @IsNumber()
     @Min(0)
     public offset?: number = 0;
 
     @Field({nullable: true, description: 'Max limit of data per page.'})
+    @ValidateIf((object, value) => value !== undefined)
     @IsNumber()
-    @IsIn([10, 25, 50, 100, 150])
+    @IsIn([5, 10, 25, 50, 100, 150])
     public limit?: number = 10;
 
     constructor(data: PaginateOptions = {}) {
