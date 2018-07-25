6003297d18bdb9b34d971e1d1dc6bc6515b5affe
Ensar Kovankaya <ensar@kovankaya.com>
2018-07-25 16:43:30 +0300

    add validate if for optional fields

--- app/src/graphql/args/role.query.ts
+++ app/src/graphql/args/role.query.ts
@@ -1,4 +1,4 @@
-import { IsArray, IsIn } from "class-validator";
+import { IsArray, IsIn, ValidateIf } from "class-validator";
 import { Field, InputType } from 'type-graphql';
 import { Role } from '../../models/user.model';
 import { Validatable } from '../validatable';
@@ -6,13 +6,15 @@ import { Validatable } from '../validatable';
 @InputType({description: 'Role Query'})
 export class RoleQuery extends Validatable {
     @Field(type => Role, {nullable: true, description: 'User role equals to given value.'})
+    @ValidateIf((object, value) => value !== undefined)
     @IsIn([Role.ADMIN, Role.MANAGER, Role.INSTRUCTOR, Role.PARENT, Role.STUDENT], {message: 'Invalid'})
     public eq?: Role;
 
     @Field(type => [Role], {nullable: true, description: 'User role is one of given values.'})
+    @ValidateIf((object, value) => value !== undefined)
     @IsArray()
     @IsIn([Role.ADMIN, Role.MANAGER, Role.INSTRUCTOR, Role.PARENT, Role.STUDENT], {message: 'Invalid', each: true})
-    public in: Role[];
+    public in?: Role[];
 
     constructor(data = {}) {
         super(data);
