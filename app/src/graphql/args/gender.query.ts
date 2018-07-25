123dbbfe450745d34926e04b4c2583614fc087b8
Ensar Kovankaya <ensar@kovankaya.com>
2018-07-25 16:42:25 +0300

    make 'in' field optional

--- app/src/graphql/args/gender.query.ts
+++ app/src/graphql/args/gender.query.ts
@@ -13,7 +13,7 @@ export class GenderQuery extends Validatable implements IGenderQueryModel {
     @Field(type => [Gender], {nullable: true, description: 'User gener is one of given values.'})
     @IsArray()
     @IsIn([Gender.MALE, Gender.FEMALE, Gender.UNKNOWN], {message: 'Invalid', each: true})
-    public in: Gender[];
+    public in?: Gender[];
 
     constructor(data = {}) {
         super(data);
