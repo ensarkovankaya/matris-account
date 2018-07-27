f19cb11d2f0a384909af834d7fa31311fb216bc0
Ensar Kovankaya <ensar@kovankaya.com>
2018-07-28 00:41:49 +0300

    add reset route

--- app/test/server/index.ts
+++ app/test/server/index.ts
@@ -10,18 +10,41 @@ const PORT = parseInt(process.env.PORT || '3000', 10);
 const HOST = process.env.HOST || '0.0.0.0';
 
 const PATH: string = process.env.MOCK_DATA || __dirname + '/../data/db.json';
-const DATA = JSON.parse(readFileSync(PATH, {encoding: 'utf8'}));
+const DATA = JSON.parse(readFileSync(PATH, { encoding: 'utf8' }));
 
 const database = new MockDatabase();
 
 const express = new Server();
-const server = createServer(express.app);
+/**
+ * Resets database data.
+ * @param req Request
+ * @param res Response
+ */
+const resetRoute = async (req, res) => {
+    try {
+        await database.load(DATA);
+        return res.status(200).send({ message: 'Data Loaded' });
+    } catch (e) {
+        console.error('Database reset failed!', e);
+        return res.status(500).send({ message: 'Server Error' });
+    }
+};
 
-database.load(DATA).then(() => {
-    console.info('Mock data loaded.');
-    Container.set(DatabaseService, database);
-    server.listen(PORT, HOST, () => console.info(`Test Server start on host ${HOST} port ${PORT}.`));
-}).catch(error => {
-    console.error('Mock data can not loaded', error);
-    throw error;
-});
+express.app.use('/reset', resetRoute);
+
+const server: HttpServer = createServer(express.app);
+
+const initialize = async () => {
+    try {
+        console.info('Mock data loading...');
+        await database.load(DATA);
+        console.info('Mock data loaded.');
+        Container.set(DatabaseService, database);
+        return server.listen(PORT, HOST, () => console.info(`Test Server start on host ${HOST} port ${PORT}.`));
+    } catch (e) {
+        console.error('Mock data can not loaded', e);
+        throw e;
+    }
+};
+
+initialize();
