9968df9742862adf8a6a8e2eba975e9528479711
Ensar Kovankaya <ensar@kovankaya.com>
2018-08-21 16:43:25 +0300

    add more comment

--- app/src/index.ts
+++ app/src/index.ts
@@ -5,19 +5,27 @@ import { Server } from "./server";
 
 const logger = getLogger('bootstrap');
 
-const bootstrap = async () => {
+/**
+ * Initialize server, connect database and run app.
+ * @param port: Port to listen to
+ * @param host: Host to listen to
+ */
+const bootstrap = async (port: number, host: string) => {
+    // Init express server
     const express = new Server();
+
+    // Connect to database
     await express.connect();
 
-    const port = parseInt(process.env.PORT || '3000', 10);
-    const host = process.env.HOST || '0.0.0.0';
-
+    // Create http server
     const server = http.createServer(express.app);
 
+    // Listen
     server.listen(port, host, () => logger.info(`Server listening on host ${host} port ${port}.`, {host, port}));
 };
 
-bootstrap()
+// Run server
+bootstrap(parseInt(process.env.PORT || '3000', 10), process.env.HOST || '0.0.0.0')
     .catch(err => {
         logger.error('Server starting failed.', err);
         process.exit(1);
